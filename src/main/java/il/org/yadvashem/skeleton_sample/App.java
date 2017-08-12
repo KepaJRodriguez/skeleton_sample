f3b2e483dfbaec46f568507646f6d8390baba7ce
KepaJRodriguez <kepa.rodriguez@gmail.com>
2017-08-12 21:51:36 +0300

    First commit

---
+++ src/main/java/il/org/yadvashem/skeleton_sample/App.java
@@ -0,0 +1,21 @@
+package il.org.yadvashem.skeleton_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	
+    	String table_mappings = args[0];
+    	String folder_input = args[1];
+    	String folder_result = folder_input + "_result";
+    	
+    	
+    	
+    	
+    }
+}
